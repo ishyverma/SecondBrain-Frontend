f1d9950c5133568c2610302c2076fe82149174fd
ishyverma <ishyamver@gmail.com>
2024-11-22 16:21:07 +0530

    init

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+    variant: "primary" | "secondary";
+    size: "sm" | "md" | "lg";
+    text: string;
+    startIcon?: any;
+    endIcon?: any;
+    onClick: () => void;
+}
+
+export const Button = ( props: ButtonProps ) => {
+    return <div>
+
+    </div>
+}
